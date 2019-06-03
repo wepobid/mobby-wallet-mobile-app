8eb1b2d7cfcf8344198530d8535ed4e14ea9c51f
Nikita Zakharov <nikitarui92@gmail.com>
2019-06-03 18:10:39 +0300

    update wallet

--- www/service-worker.js
+++ www/service-worker.js
@@ -15,7 +15,7 @@ importScripts("workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
 
 importScripts(
-  "precache-manifest.069b4e429590d8519afd1d0ec254a8ca.js"
+  "precache-manifest.fd0e2ade0b34f8cfb228e7b64e401806.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "myetherwallet"});
